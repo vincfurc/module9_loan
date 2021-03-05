32811b88032d85a56500c07d4a8469c1fda2b550
vfurci200 <furc93@hotmail.it>
2021-03-05 17:09:03 +0100

    Add files via upload

---
+++ contracts/loan.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.6.6;
+
+/**
+ *@title loan
+ *@dev borrow and loan money
+ */
+
+contract Loan is  {
+
+    constructor() public {}
+    
+ /**
+ *@param the amount to be borrowed
+ */
+ 
+ function deposit(uint256 amount) public {
+ }
+ 
+/**
+ *@param the amount to be borrowed
+ */
+ 
+ 
+ function borrow(uint256 amount) public {
+ }
+ 
+/**
+ *@param the amount to be borrowed
+ */
+ 
+ function withdraw(uint256 amount, address to) public {
+ }
+ 
+ 
+
+
+}
